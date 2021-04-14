fb6391411b81cbf02951846f209734d855e7f83a
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-14 09:30:21 -0500

    Update and rename basicmate.py to mate.py

---
+++ scienlib/mate.py
@@ -0,0 +1,22 @@
+import math
+
+class Basic:
+
+      def velocity(d,t):
+         return d / t
+
+      def divider(n,c):
+         residue = n % c
+         if residue == 0:
+             return True
+         else:
+             return False
+
+      def average(l):
+         sum = 0
+         for element in l:
+             sum += element
+
+         average = sum / len (l)
+         return average
+       
