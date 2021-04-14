667ca0e552293abac0e522ffd5e68c1cbbced03e
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-14 09:42:13 -0500

    Create red.py

---
+++ scienlib/red.py
@@ -0,0 +1,10 @@
+import math
+
+class Red:
+
+      def __init__(self):
+             #print ("Hello World")
+             return
+
+      def sigmoide (z):
+             return (1/(1+math.exp (-z))
