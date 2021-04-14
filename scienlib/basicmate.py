18c34af2f69bc2554f12ee1af5aaa1b116dbbc1a
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-13 20:45:42 -0500

    Update basicmate.py

--- scienlib/basicmate.py
+++ scienlib/basicmate.py
@@ -1,3 +1,10 @@
 def velocity(d,t):
        return d / t
+
+def divider(n,c):
+       residue = n % c
+       if residue == 0:
+             return True
+       else:
+             return False
        
