c34aacc1da31fb0ce676b30cb5e09fc6184b0651
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-14 09:37:32 -0500

    Update mate.py

--- scienlib/mate.py
+++ scienlib/mate.py
@@ -2,17 +2,26 @@ import math
 
 class Basic:
 
-      def velocity(d,t):
+      def __init__(self):
+         #print ("Hello World")
+         return
+
+      def velocity(self,d,t):
+         self.d = d
+         self.t = t
          return d / t
 
-      def divider(n,c):
+      def divider(self,n,c):
+         self.n = n
+         self.c = c
          residue = n % c
          if residue == 0:
              return True
          else:
              return False
 
-      def average(l):
+      def average(self,l):
+         self.l = l
          sum = 0
          for element in l:
              sum += element
